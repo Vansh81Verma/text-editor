b550947dd361ec68897810b39f542fbe7bbfc0d9
Vansh81Verma <121802408+Vansh81Verma@users.noreply.github.com>
2023-05-14 12:51:48 +0530

    Add files via upload

---
+++ Text Editor/script.js
@@ -0,0 +1,30 @@
+function bt1(){
+    document.getElementById("textarea1").style.fontWeight="bold";
+}
+function bt3(){
+    document.getElementById("textarea1").style.textAlign="center";
+}
+function bt4(){
+    document.getElementById("textarea1").style.textAlign="justify";
+}
+function bt5(){
+    document.getElementById("textarea1").style.textAlign="right";
+}
+function bt6(){
+    document.getElementById("textarea1").style.textAlign="left";
+}
+function bt8(){
+    document.getElementById("textarea1").style.textTransform="uppercase";
+}
+function bt9(){
+    document.getElementById("textarea1").style.textTransform="lowercase";
+}
+function bt7(){
+    document.getElementById("textarea1").style.fontStyle="italic";
+}
+function bt2(){
+    document.getElementById("textarea1").style.textTransform="capitalize";
+}
+function bt11(){
+    document.getElementById("textarea1").value=" ";
+}
